619d0aef959604d55cc7e917eb69c45d72658188
seoyun <parkysys17@gmail.com>
2023-11-15 21:43:59 +0900

    plz

---
+++ eng_back/src/main/java/com/project/eng_back/EngBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.eng_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngBackApplication.class, args);
+	}
+
+}
