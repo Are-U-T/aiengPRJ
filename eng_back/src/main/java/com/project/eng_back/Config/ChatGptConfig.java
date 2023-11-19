c3170999c0bfa7d020ca3c99f4166a81692c832a
seoyun <parkysys17@gmail.com>
2023-11-19 15:18:01 +0900

    멍청이가 결제함 ;;

--- eng_back/src/main/java/com/project/eng_back/Config/ChatGptConfig.java
+++ eng_back/src/main/java/com/project/eng_back/Config/ChatGptConfig.java
@@ -3,7 +3,7 @@ package com.project.eng_back.Config;
 public class ChatGptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "깃허브에 key 값 올리면 자꾸 유출된다고 key 값 삭제하니까,, 이렇게 올릴게요";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
