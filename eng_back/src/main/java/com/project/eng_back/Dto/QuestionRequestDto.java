2e099bd38e0737b6c42f53f4bbd93707309b4a9f
seoyun <parkysys17@gmail.com>
2023-11-19 13:50:46 +0900

    멍청이가 결제함 ;;

---
+++ eng_back/src/main/java/com/project/eng_back/Dto/QuestionRequestDto.java
@@ -0,0 +1,10 @@
+package com.project.eng_back.Dto;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class QuestionRequestDto implements Serializable {
+    private String question;
+}
