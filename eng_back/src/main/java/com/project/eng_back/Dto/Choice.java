c8feda225ec3d296d96862e61b05d1bcd7ee7452
jdaily00 <kyeonghee_@naver.com>
2023-11-23 14:59:35 +0900

    TalkingRoom data send -> ChatGpt

---
+++ eng_back/src/main/java/com/project/eng_back/Dto/Choice.java
@@ -0,0 +1,31 @@
+package com.project.eng_back.Dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor
+public class Choice implements Serializable {
+
+    private String text;
+    private Integer index;
+    @JsonProperty("finish_reason")
+    private String finishReason;
+
+    private int sequence;
+
+    public void setSequence(int sequence) {
+        this.sequence = sequence;
+    }
+
+    @Builder
+    public Choice(String text, Integer index, String finishReason) {
+        this.text = text;
+        this.index = index;
+        this.finishReason = finishReason;
+    }
+}
---
+++ eng_back/src/main/java/com/project/eng_back/Dto/QuestionRequestDto.java
@@ -0,0 +1,62 @@
+package com.project.eng_back.Dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class QuestionRequestDto implements Serializable {
+
+    private String question;
+
+    private int sequence;
+
+    @JsonProperty("gptRole")
+    private String GPTRole;
+
+    @JsonProperty("userRole")
+    private String UserRole;
+
+    private String situation;
+
+    private String crid;
+
+    public QuestionRequestDto() {
+    }
+
+    @JsonCreator
+    public QuestionRequestDto(@JsonProperty("question") String question) {
+        this.question = question;
+    }
+
+    public QuestionRequestDto(String question, String GPTRole, String UserRole, String situation) {
+        this.question = question;
+        this.GPTRole = GPTRole;
+        this.UserRole = UserRole;
+        this.situation = situation;
+    }
+
+    public void setSequence(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setGPTRole(String GPTRole) {
+        this.GPTRole = GPTRole;
+    }
+
+    public void setUserRole(String UserRole) {
+        this.UserRole = UserRole;
+    }
+
+    public void setSituation(String situation) {
+        this.situation = situation;
+    }
+
+    public void setCrid(String crid) { this.crid = crid; }
+}
