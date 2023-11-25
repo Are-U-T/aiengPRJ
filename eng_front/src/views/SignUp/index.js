a84d493c28856bd7e99e6ce6956bbf9318eb6c01
jdaily00 <kyeonghee_@naver.com>
2023-11-25 20:13:24 +0900

    login and sign up test done

--- eng_front/src/views/SignUp/index.js
+++ eng_front/src/views/SignUp/index.js
@@ -153,6 +153,7 @@ export default function SignInSide() {
                                         name="mail"
                                         autoComplete="mail"
                                         autoFocus
+                                        value="kyeonghee_@naver.com"
                                     />
 
                                     <Button // 인증 버튼 (인증번호 메일 요청)
