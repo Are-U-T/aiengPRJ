05208bfd058b0340e8f821d216100236c80219d7
park-seoyun-620 <p815000@naver.com>
2023-12-07 00:09:19 +0900

    12/07 -> 1

--- eng_front/src/views/MainHome/index.js
+++ eng_front/src/views/MainHome/index.js
@@ -1,17 +1,18 @@
 import React, {useEffect, useState} from "react";
-import Navigation from "../../../../../../../Desktop/옮기는용/Navigation";
+import Navigation from "../Navigation";
 import Footer from "./Footer"
 import Mainarea from "./Mainarea";
 import Header from "./Header";
+import '../../App.css'
 
 export default function MainHome(){
 
     return (
-        <>
+        <div className='App'>
             <Navigation/>
             <Header/>
             <Mainarea/>
             <Footer/>
-        </>
+        </div>
     )
 }
