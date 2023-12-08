f015d99b4fb6ab586bbb0877f4bad2bd8c1806de
chelling2 <junghun_1999@naver.com>
2023-12-08 22:29:37 +0900

    12!!!!
    12!!!!!

--- eng_back/src/main/java/com/project/eng_back/Controller/TalkingRoomController.java
+++ eng_back/src/main/java/com/project/eng_back/Controller/TalkingRoomController.java
@@ -145,7 +145,7 @@ public class TalkingRoomController {
                 gptRole = "boyfriend";
                 userRole = "girfriend";
                 if(country.equals("영국")){
-                    voice = 4;
+                    voice = 3;
                 } else {
                     voice = 7;
                 }
@@ -175,9 +175,9 @@ public class TalkingRoomController {
                 gptRole = "man";
                 userRole = "woman";
                 if(country.equals("영국")){
-                    voice = 1;
+                    voice = 3;
                 } else {
-                    voice = 5;
+                    voice = 7;
                 }
             } else{
                 gptRole = "woman";
